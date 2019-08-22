5e9381bc6158eb97770cf503e3c0f8667088914a
Donovan Dikaio <i@dikaio.com>
2019-08-21 21:07:09 -0700

    Prep apps.sh

---
+++ apps.sh
@@ -0,0 +1,48 @@
+# brewl.sh
+# Author: Donovan Dikaio <github@dikaio.com>
+# Source: https://github.com/dikaio/dotfiles
+# =============================================================================
+# Install apps available via Homebrew!
+
+# run as administrator
+sudo -v
+
+sh config/cask.sh
+
+brew cask install 1password
+brew cask install appcleaner
+brew cask install aws-vault
+brew cask install basictex
+brew cask install battle-net
+brew cask install caffeine
+brew cask install cryptomator
+brew cask install dash
+brew cask install docker
+brew cask install dnsmasq
+brew cask install docker
+brew cask install downie
+brew cask install dropbox
+brew cask install espionage
+brew cask install espresso
+brew cask install firefox
+brew cask install firefox-developer-edition
+brew cask install flux
+brew cask install google-chrome
+brew cask install gpg-suite
+brew cask install iterm2
+brew cask install java
+brew cask install nordvpn
+brew cask install permute
+brew cask install sequel-pro
+brew cask install screaming-frog-seo-spider
+brew cask install signal
+brew cask install sketch
+brew cask install slack
+brew cask install spotify
+brew cask install sublime-text
+brew cask install the-unarchiver
+brew cask install transmission
+brew cask install transmit
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install visual-studio-code
